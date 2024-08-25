d45230f9b5e4908aee0897bef13192451621af98
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-08-24 19:58:32 -0600

    Momement done

--- Assets/BotInstantiator.cs
+++ Assets/BotInstantiator.cs
@@ -5,30 +5,100 @@ using UnityEngine.UIElements;
 
 public class BotInstantiator : MonoBehaviour
 {
+    [Header("Robot settings")]
     public GameObject botPrefab;
     [SerializeField] int botAmount;
-    [SerializeField] float minRange = 3.0f;
-    [SerializeField] float maxRange = 10.0f;
-    // Start is called before the first frame update
+    [Tooltip("Velocity min range")]
+    [SerializeField] float minRange = 0.3f;
+    [Tooltip("Velocity max range")]
+    [SerializeField] float maxRange = 1.5f;
+    
+    [Header("Box settings")]
+    public GameObject boxPrefab;
+    [SerializeField] int boxAmount;
+
+    [Header("Storage box settings")]
+    public GameObject storagePrefab;
+    [SerializeField] int binAmount;
+
+    Dictionary<string, Vector3> positionTracker = new Dictionary<string, Vector3>();
     void Start()
     {
+        BotSpawner();
+        BoxSpawner();
+        
+    }
+
+    void BotSpawner(){
         for (int i = 0; i < botAmount; i++){
             GameObject currentbot = Instantiate(botPrefab);
             RobotMovementManager currentBotScript = currentbot.GetComponent<RobotMovementManager>();
 
             float velocity = Random.Range(minRange, maxRange);
-            int colorStarter = Random.Range(1,3);
-            Vector3 position = new Vector3(Random.Range(-4f,4f), 0, Random.Range(-7f,7f));
+            int colorStarter = Random.Range(0,2);
+            currentbot.name = "Bot " + i;
+
+            Vector3 position = positionFinder(currentbot.name);
+            Quaternion rotation = rotationSet();
+
             if (currentBotScript != null){
-                currentBotScript.Init(velocity, colorStarter, position);
+                currentBotScript.Init(velocity, colorStarter, position, rotation);
             }
         }
-        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void BoxSpawner(){
+        for (int i = 0; i < boxAmount; i++){
+            GameObject currentBox = Instantiate(boxPrefab);
+            BoxPlacer currentBoxScript = currentBox.GetComponent<BoxPlacer>();
+            currentBox.name = "Box " + i;
+
+            Vector3 position = positionFinder(currentBox.name);
+            Quaternion rotation = rotationSet();
+
+            if (currentBoxScript != null){
+                currentBoxScript.Init(position, rotation);
+            }
+        }
+    }
+    Vector3 positionFinder(string objectName){
+        Vector3 position;
+        do{
+            position = new Vector3(
+                Mathf.Round(Random.Range(-10f, 9f)) + 0.5f, 
+                0, 
+                Mathf.Round(Random.Range(-7f, 6f)) + 0.5f
+            );
+        }while(positionTracker.ContainsValue(position));
+
+        if (objectName.StartsWith("Box")){
+            position.y += 0.5f;
+        }
+
+        positionTracker.Add(objectName,position);
+        return position;
+    }
+
+    Quaternion rotationSet(){
+        int randInt = Random.Range(1,5);
+        Quaternion rotation;
+        switch(randInt){
+            case 1:
+                rotation = Quaternion.Euler(0,0,0);
+                break;
+            case 2:
+                rotation = Quaternion.Euler(0,90,0);
+                break;
+            case 3:
+                rotation = Quaternion.Euler(0,180,0);
+                break;
+            case 4:
+                rotation = Quaternion.Euler(0,270,0);
+                break;
+            default:
+                rotation = Quaternion.Euler(0, 0, 0);
+                break;
+        }
+        return rotation;
     }
 }
--- Assets/Robot/RobotMovementManager.cs
+++ Assets/Robot/RobotMovementManager.cs
@@ -8,32 +8,55 @@ public class RobotMovementManager : MonoBehaviour
 {
     Rigidbody botRigid;
     [SerializeField] GameObject center;
-    [SerializeField] SpotLight siren;
-    public int randomColorStart;
+    public Light siren;
+    public int randomColorStart = 0;
     public float velocity;
+    private Color[] colors;
+    public float sirenInterval = 0.5f;
     private float raySize = 1.0f;
+    private float distanceMoved = 0f;
 
-    public void Init(float _velocity, int _randomColorStart, Vector3 _position)
-    {
+    public void Init(float _velocity, int _randomColorStart, Vector3 _position, Quaternion _rotation){
         velocity = _velocity;
         randomColorStart = _randomColorStart;
         transform.position = _position;
+        transform.rotation = _rotation;
     }
     
     void Start()
     {
-        
+        botRigid = GetComponent<Rigidbody>();
+        colors = new Color[2];
+        colors[0] = new Color(1,0,0);
+        colors[1] = new Color(1,1,1);
+        StartCoroutine(ChangeColorRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    IEnumerator ChangeColorRoutine(){
+        while(true){
+            siren.color = colors[randomColorStart];
+            randomColorStart = (randomColorStart + 1) % colors.Length;
+            yield return new WaitForSeconds(sirenInterval);
+        }
+    }
+
+    void FixedUpdate(){
+        float movement = velocity * Time.deltaTime;
+        distanceMoved += movement;
+        if (distanceMoved >= 0.5f)
+        {
+            float overshoot = distanceMoved - 0.5f;
+            movement -= overshoot;
+            distanceMoved = 0f;
+        }
+
+        botRigid.MovePosition(botRigid.position + transform.forward * movement);
+    }
+    void Update(){
         
         Vector3 p1 = center.transform.position;
 
-        // Forward Ray
-        if (Physics.Raycast(p1, center.transform.forward, out RaycastHit hitForward, raySize))
-        {
+        if (Physics.Raycast(p1, center.transform.forward, out RaycastHit hitForward, raySize)){
             if (hitForward.collider.gameObject != this.gameObject)
             {
                 string hitTagForward = hitForward.collider.gameObject.tag;
@@ -41,9 +64,7 @@ public class RobotMovementManager : MonoBehaviour
             }
         }
 
-        // Backward Ray
-        if (Physics.Raycast(p1, -center.transform.forward, out RaycastHit hitBackward, raySize))
-        {
+        if (Physics.Raycast(p1, -center.transform.forward, out RaycastHit hitBackward, raySize)){
             if (hitBackward.collider.gameObject != this.gameObject)
             {
                 string hitTagBackward = hitBackward.collider.gameObject.tag;
@@ -51,9 +72,7 @@ public class RobotMovementManager : MonoBehaviour
             }
         }
 
-        // Left Ray
-        if (Physics.Raycast(p1, -center.transform.right, out RaycastHit hitLeft, raySize))
-        {
+        if (Physics.Raycast(p1, -center.transform.right, out RaycastHit hitLeft, raySize)){
             if (hitLeft.collider.gameObject != this.gameObject)
             {
                 string hitTagLeft = hitLeft.collider.gameObject.tag;
@@ -61,9 +80,7 @@ public class RobotMovementManager : MonoBehaviour
             }
         }
 
-        // Right Ray
-        if (Physics.Raycast(p1, center.transform.right, out RaycastHit hitRight, raySize))
-        {
+        if (Physics.Raycast(p1, center.transform.right, out RaycastHit hitRight, raySize)){
             if (hitRight.collider.gameObject != this.gameObject)
             {
                 string hitTagRight = hitRight.collider.gameObject.tag;
@@ -71,7 +88,6 @@ public class RobotMovementManager : MonoBehaviour
             }
         }
 
-        // Debug Ray Visualization
         Debug.DrawRay(p1, center.transform.forward * raySize, Color.red);
         Debug.DrawRay(p1, -center.transform.forward * raySize, Color.blue);
         Debug.DrawRay(p1, -center.transform.right * raySize, Color.green);
